3e45bc3d8751e664fa25cba8f21bcc8cd161d6c1
sonali4596 <sonalianmol4596@gmail.com>
2025-10-28 01:20:12 +0530

    Implement solution for counting laser beams in bank

---
+++ Number of Laser Beams in a Bank.java
@@ -0,0 +1,17 @@
+//2125. Number of Laser Beams in a Bank
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        long beams=0;
+        long prevd=0;
+        long currd=0;
+        prevd = bank[0].chars().filter(c -> c == '1').count();
+        for(int i=1;i<bank.length;i++)
+        {
+            currd=bank[i].chars().filter(c->c=='1').count();
+            if(currd!=0 &&prevd!=0)
+                beams+=currd*prevd;
+            if(currd!=0)prevd=currd;
+        }
+        return (int)beams;
+    }
+}
