762d4afdcf89fb05290d800bb4598d88e561c750
sonali4596 <sonalianmol4596@gmail.com>
2025-10-29 16:12:04 +0530

    Update Smallest Number With All Set Bits.java

--- Smallest Number With All Set Bits.java
+++ Smallest Number With All Set Bits.java
@@ -1,4 +1,6 @@
 //3370. Smallest Number With All Set Bits
+//Time: O(1) (bit ops)
+//Space: O(1)
 class Solution {
     public int smallestNumber(int n) {
         int k = Integer.SIZE - Integer.numberOfLeadingZeros(n); 
