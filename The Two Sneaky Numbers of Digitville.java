4ae7dee5aa3123677536be6bc7d5ca784a2c6908
sonali4596 <sonalianmol4596@gmail.com>
2025-10-31 07:05:08 +0530

    Add solution for finding sneaky numbers in an array

---
+++ The Two Sneaky Numbers of Digitville.java
@@ -0,0 +1,18 @@
+//3289. The Two Sneaky Numbers of Digitville
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int[] ans=new int[2];
+        HashMap<Integer,Integer>mp=new HashMap<Integer,Integer>();
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(mp.containsKey(nums[i]))
+            {
+                ans[k]=nums[i];
+                k++;
+            }
+            else mp.put(nums[i],1);
+        }
+        return ans;
+    }
+}
