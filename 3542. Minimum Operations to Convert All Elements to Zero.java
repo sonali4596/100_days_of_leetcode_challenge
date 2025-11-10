c14ec1a2a09ffe2d8c11d7018cb275498bd75d82
sonali4596 <sonalianmol4596@gmail.com>
2025-11-10 23:03:20 +0530

    Create 3542. Minimum Operations to Convert All Elements to Zero.java

---
+++ 3542. Minimum Operations to Convert All Elements to Zero.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        Deque<Integer> st = new ArrayDeque<>();
+        st.push(0);
+
+        for (int num : nums) {
+            
+            while (!st.isEmpty() && st.peek() > num) {
+                st.pop();
+            }
+
+            if (st.isEmpty() || st.peek() < num) {
+                if (num > 0) {       // zero doesn't need its own op
+                    ans++;
+                }
+                st.push(num);
+            }
+            
+        }
+
+        return ans;
+    }
+}
